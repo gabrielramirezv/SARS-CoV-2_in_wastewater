a87eadc87fa799018e6feb371f9ff60b5bc12ad3
Gabriel Ramirez Vilchis <gramirez@lcg.unam.mx>
2024-11-17 22:08:48 -0600

    File renamed

--- src/map.py
+++ src/map.py
@@ -1,56 +1,91 @@
-import matplotlib.pyplot as plt
+'''
+NAME
+    map
+
+VERSION
+    1.0
+
+AUTHOR
+    Santiago Orozco & Gabriel Ramirez
+
+GITHUB
+    https://github.com/gabrielramirezv/SARS-CoV-2_in_wastewater/blob/main/src/map.py
+
+DESCRIPTION
+    Plot locations in a map by clusters
+
+CATEGORY
+    location analysis
+
+USAGE
+    python map.py
+
+ARGUMENTS
+    None
+
+SEE ALSO
+    k_means
+    cluster_analysis
+
+'''
+
+# Import libraries
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
+from k_means_v1 import uk_cluter
 from mpl_toolkits.basemap import Basemap
 
-from k_means_v1 import uk_cluter
+# ============================================================================
 
-# =================================================================================================
-
-
-# Lista de coordenadas (latitud, longitud)
+# Read coordinates list
 co = pd.read_csv("../data/monitoring-sites.csv")
 
+# Generate the cluster by location
 uk_cluter = uk_cluter.reset_index()
 
-coords_merged = pd.merge(uk_cluter[["Site_ID", "Cluster"]], co[["site_id", "latitude", "longitude"]], left_on='Site_ID', right_on='site_id', how='inner')
+# Merge coordinates
+coords_merged = pd.merge(uk_cluter[["Site_ID", "Cluster"]], 
+                         co[["site_id", "latitude", "longitude"]], 
+                         left_on='Site_ID', 
+                         right_on='site_id', 
+                         how='inner')
 
-
+# Get each cluster
 cluster_0 = coords_merged[coords_merged.Cluster == 0]
-
 cluster_1 = coords_merged[coords_merged.Cluster == 1]
-
 cluster_2 = coords_merged[coords_merged.Cluster == 2]
 
-# =================================================================================================
+# ============================================================================
 
 plt.figure(figsize=(8, 8))
 
-# Crear el mapa con limites ajustados para enfocar Inglaterra
-m = Basemap(
-    projection='merc', 
-    llcrnrlat=54, urcrnrlat=61,   # Latitud
-    llcrnrlon=-8, urcrnrlon=2,    #  longitud
-    resolution='i'
-)
+# Create the map with UK coordinates
+m = Basemap(projection ='merc', 
+            llcrnrlat = 54, urcrnrlat=61, 
+            llcrnrlon=-8, urcrnrlon=2, 
+            resolution='i')
+
+# Define map characteristics
 m.drawcoastlines()
 m.drawcountries()
 m.drawmapboundary(fill_color='lightblue')
 m.fillcontinents(color='lightgray', lake_color='lightblue')
 
-# Convertir las coordenadas al sistema de coordenadas del mapa y graficarlas
+# Convert cluster 0 coordinates to map coordinates system and plot them
 for index, row in cluster_0.iterrows():
     x, y = m(row['longitude'], row['latitude'])
     m.plot(x, y, 'bo', markersize=4) 
 
+# Convert cluster 1 coordinates to map coordinates system and plot them
 for index, row in cluster_1.iterrows():
     x, y = m(row['longitude'], row['latitude'])
     m.plot(x, y, 'go', markersize=4) 
 
+# Convert cluster 2 coordinates to map coordinates system and plot them
 for index, row in cluster_2.iterrows():
     x, y = m(row['longitude'], row['latitude'])
     m.plot(x, y, 'ro', markersize=4) 
 
-# Mostrar el mapa
-plt.title('Mapa de Coordenadas Geograficas')
+# Show plot
+plt.title('Geographic Coordinates Map')
 plt.show()
